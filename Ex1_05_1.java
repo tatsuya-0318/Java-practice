703cbdd396ac07f949d5b7fc52f028eb9ea140a2
tatsuya-0318 <146906978+tatsuya-0318@users.noreply.github.com>
2024-01-29 16:58:25 +0900

    セクション5　～完～

---
+++ Ex1_05_1.java
@@ -0,0 +1,25 @@
+class Ex1_05_1 {
+	public static void main (String[] args) {
+		
+		int lifetime_wages_in_the_IT_industry = 276000000 ;  //IT業界の生涯賃金
+		
+		long iT_industry_market_size  = 631500000000L ;  //IT業界の市場規模
+		
+		char favorite_kanji = '森' ; //自分の好きな漢字1文字
+		
+		String the_profession_you_want_to_be = "ゲームクリエイター系VTuber" ; //自分のなりたい職業
+		
+		Boolean Whether_you_are_a_graduate_or_not = true ; //自分が既卒かどうか
+		
+		System.out.println(lifetime_wages_in_the_IT_industry);
+			
+		System.out.println(iT_industry_market_size);
+
+		System.out.println(favorite_kanji);
+
+		System.out.println(the_profession_you_want_to_be);
+
+		System.out.println(Whether_you_are_a_graduate_or_not);
+
+	}
+}
---
+++ Sample1_04_1.java
@@ -0,0 +1,13 @@
+class Sample1_04_1 {
+	public static void main (String[] args) {
+		
+		int    teika   = 1000 ;             
+		
+		double zeiritu = 1.08 ;             
+		
+		double siharai = teika * zeiritu ;  
+		
+		System.out.println(siharai) ;       
+		
+	}
+}
