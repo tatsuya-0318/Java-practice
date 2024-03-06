f5691a6fdd60e6b490892fb6048696355fc5fb79
tatsuya-0318 <146906978+tatsuya-0318@users.noreply.github.com>
2024-03-06 19:12:52 +0900

    せくしょん9

--- Sample1_09_1.java
+++ Sample1_09_1.java
@@ -6,10 +6,7 @@ class Sample1_09_1 {
 		int[][] rooms = {{101, 102, 103}, {201, 202, 203}, {301, 302, 303}} ;
 		
 		System.out.println("rooms:" + rooms );
-		
-		
-		
-		
+
 		String  name   = "moco" ;
 		
 		boolean check1 = name == args[0] ;
